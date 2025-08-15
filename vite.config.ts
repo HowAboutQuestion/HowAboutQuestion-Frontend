7718041494dea3d41573789b54ddccccd8873796
khael <khaelim1311@gmail.com>
2025-08-16 05:58:04 +0900

    feat: Set SCSS setting

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/colors" as *;`  
-      }
+        additionalData: `@use "@/styles/index" as *;`,
+      },
     },
   },
 });
