73adb759e1a1ad0dff59cb3591a421de0269f2de
최은창 <cod0216@nate.com>
2026-05-12 16:59:24 +0900

    chore: LoginTest remove and welcompage router modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router';
-import LoginTest from '@/pages/user/LoginTest';
 import Welcome from '@/pages/welcome/Welcome';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      <Route path="/LoginTest" element={<LoginTest />} />
     </Routes>
   );
 };
